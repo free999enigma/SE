618fc57592a1cc1ead524ecb02a88195782ac2c5
free999enigma <free999enigma@gmail.com>
2023-02-09 19:17:55 +0200

    update

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="home">
     <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
-      <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
+      <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-center overflow-y-hidden">
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
 
         <div class="mb-12 xl:mb-0">
